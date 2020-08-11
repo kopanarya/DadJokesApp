9df60aa8a12ba479e4a15eef31e28ec5eeed9f45
alikurtulush92 <kopanarya92@gmail.com>
2020-08-11 13:22:42 +0100

    Updates

---
+++ precache-manifest.2181af64ff6e959bafd02b7a5c5255df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89dd69f72595efaf1248b45398adddec",
+    "url": "/DadJokesApp/index.html"
+  },
+  {
+    "revision": "fb54aab1068ab64af816",
+    "url": "/DadJokesApp/static/css/main.c93e0e4a.chunk.css"
+  },
+  {
+    "revision": "12c03b9a59dfe8fd353d",
+    "url": "/DadJokesApp/static/js/2.877e69a2.chunk.js"
+  },
+  {
+    "revision": "73f631d49e0419a420c7f58042b4e6ae",
+    "url": "/DadJokesApp/static/js/2.877e69a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb54aab1068ab64af816",
+    "url": "/DadJokesApp/static/js/main.fde1b415.chunk.js"
+  },
+  {
+    "revision": "3ffc06663284f9700ae8",
+    "url": "/DadJokesApp/static/js/runtime-main.beaea540.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/DadJokesApp/static/media/logo.25bf045c.svg"
+  }
+]);
